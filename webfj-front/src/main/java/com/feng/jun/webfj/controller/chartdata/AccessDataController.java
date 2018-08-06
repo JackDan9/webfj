0eda6bad13b66fc5a641c3d4165f9107c4338264
JackDan9 <1835812864@qq.com>
2018-08-06 22:05:08 +0800

    :cn: webfj : background_project : mygirl : webfj-front : jackdan9

--- webfj-front/src/main/java/com/feng/jun/webfj/controller/chartdata/AccessDataController.java
+++ webfj-front/src/main/java/com/feng/jun/webfj/controller/chartdata/AccessDataController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import com.feng.jun.webfj.common.constant.BFConstant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feng.jun.webfj.common.base.BaseController;
-import com.feng.jun.webfj.common.constant.BFContent;
+import com.feng.jun.webfj.common.constant.BFConstant;
 
 /**
  * 图标数据: 访问来源
@@ -30,5 +31,40 @@ import com.feng.jun.webfj.common.constant.BFContent;
 @RestController
 @RequestMapping(value = "/addatas")
 public class AccessDataController extends BaseController {
+    /**
+     * 曲线图数据模型，一般是从数据库中获取的，这里直接模拟产生
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = "applications/json; charset=utf-8")
+    public ResponseEntity<Map<String, Object>> getAdData()
+    {
+        System.out.println(BFConstant.HELLO_MSG);
+        Map<String, Object> map = new HashMap<String, Object>();
+        List<Integer> emailList = new ArrayList<Integer>();
+        List<Integer> allianceList = new ArrayList<Integer>();
+        List<Integer> vedioList = new ArrayList<Integer>();
+        List<Integer> directList = new ArrayList<Integer>();
+        List<Integer> searchList = new ArrayList<Integer>();
+        List<String> xTitleList = new ArrayList<String>();
+        Random r = new Random();
 
+        // 模拟产生数据，实际应用中请调用service获取
+        for (int i = 0; i < 60; i++)
+        {
+            emailList.add(r.nextInt(50));
+            allianceList.add(r.nextInt(50));
+            vedioList.add(r.nextInt(50));
+            directList.add(r.nextInt(50));
+            searchList.add(r.nextInt(50));
+            xTitleList.add(i + "m");
+        }
+        map.put("emailList", emailList);
+        map.put("allianceList", allianceList);
+        map.put("vedioList", vedioList);
+        map.put("directList", directList);
+        map.put("searchList", searchList);
+        map.put("xTitleList", xTitleList);
+        return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+    }
 }
