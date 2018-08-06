f253ee4db7ec97ca86d6ac01279bc6e91f9268db
JackDan9 <1835812864@qq.com>
2018-08-06 09:36:03 +0800

    :cn: webfj : background_project : mygirl : webfj-front  : jackdan9

--- webfj-front/src/main/java/com/feng/jun/webfj/controller/chartdata/AccessDataController.java
+++ webfj-front/src/main/java/com/feng/jun/webfj/controller/chartdata/AccessDataController.java
@@ -3,6 +3,32 @@ package com.feng.jun.webfj.controller.chartdata;
 /**
  * Created by JackDan9 on 2018/8/2.
  */
-public class AccessDataController {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feng.jun.webfj.common.base.BaseController;
+import com.feng.jun.webfj.common.constant.BFContent;
+
+/**
+ * 图标数据: 访问来源
+ *
+ * @author JackDan9
+ * @version webfj V1.0.0, 2018年8月5日
+ * @see
+ * @since webfj V1.0.0
+ */
+
+@RestController
+@RequestMapping(value = "/addatas")
+public class AccessDataController extends BaseController {
 
 }
